b60dd9a78a4a0545a7b982160be4c3f476173ded
Kevin <jue.kevin@myqaa.com>
2015-02-22 21:18:22 +0100

    init xcode project

---
+++ js.oi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  js.oi
+//
+//  Created by Kevin Jue on 22/02/2015.
+//  Copyright (c) 2015 Kevin Jue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
