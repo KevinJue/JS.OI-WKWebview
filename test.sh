d19794cc668e5dfb61de0bd05a279a9d4e9e4444
Kevin <jue.kevin@myqaa.com>
2015-03-22 18:48:25 +0100

    fix circle ci

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xctool \
-    -project js.oi.xcodeproj \
-    -scheme js.oi \
-    -sdk iphonesimulator \
-    build \
-    clean test -test-sdk iphonesimulator -resetSimulator -freshInstall
